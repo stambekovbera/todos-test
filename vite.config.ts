c3e00cda79473cc2aef25753d97494b202e45a56
Berador Stambekov <stambekovbera@gmail.com>
2023-09-16 02:27:08 +0500

    fix package scripts & run lint ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig( {
-  plugins: [ react() ],
-  resolve: {
-    alias: [ { find: '@', replacement: path.resolve( __dirname, 'src' ) } ],
-  },
-  server: {
-    port: 3000,
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
+    plugins: [ react() ],
+    resolve: {
+        alias: [ { find: '@', replacement: path.resolve( __dirname, 'src' ) } ],
+    },
+    server: {
+        port: 3000,
+    },
+    css: {
+        modules: {
+            localsConvention: 'camelCaseOnly',
+        }
     }
-  }
 } );
